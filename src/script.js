8a70fc44723bb4f7bf1102f92d7b0438eed4093f
lilith machine <lilith.alanis.255@gmail.com>
2023-07-16 20:48:25 -0600

    it is really speedy

--- src/script.js
+++ src/script.js
@@ -3,12 +3,10 @@ import init, {BoardGenerator} from '../pkg/loteria.js';
 async function run() {
   const wasm = await init().catch(console.error);
   let start = Date.now();
-  let generator = new BoardGenerator(4, 0);
+  let generator = new BoardGenerator(4, 4);
   let end = Date.now();
 
   console.log("delta: " + (end - start) + "ms");
-
-  generator.log_tape();
 }
 
 run();
