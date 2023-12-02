3dac65cd03746c145b6b6dd6ac18a24585aa2b80
Ulises Alanis <ulises.alanis.255@gmail.com>
2023-12-02 14:10:02 -0600

    stuff

--- src/engine/board.rs
+++ src/engine/board.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Debug};
+
 #[derive(Default, Debug, Clone, Copy)]
 pub struct BasicBoard<T>([T; 16]);
 
@@ -19,5 +21,22 @@ impl<T> std::ops::IndexMut<usize> for BasicBoard<T>{
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         let index = index * 4;
         &mut self.0[index..index+4]
+    }
+}
+
+impl<T> Display for BasicBoard<T> 
+where
+    T : Display
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for ij in 0..16{
+            let i = ij % 4;
+            write!(f, "{} ", self.0[ij])?;
+            if i == 3{
+                writeln!(f)?;
+            }
+
+        }
+        Ok(())
     }
 }
