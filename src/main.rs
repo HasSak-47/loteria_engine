19fd78657330aa7bcba0402411e9c832e529781a
arch machine <lilith.alanis.255@gmail.com>
2023-12-14 00:10:39 -0600

    fixed bug

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use loteria_engine::engine::{BoardBuilder, RandomCenterMarkPair};
 
 fn main() {
     let board = BoardBuilder::new().set_total(10).act_on(RandomCenterMarkPair);
-    println!("{board?}");
+    println!("{board:?}");
 }
