301a50773bdcb63329d248c76328b222bfbf6c9d
arch machine <lilith.alanis.255@gmail.com>
2024-04-06 15:39:39 -0600

    added stuff!

--- src/main.rs
+++
@@ -1,6 +0,0 @@
-use loteria_engine::engine::{BoardBuilder, RandomCenterMarkPair};
-
-fn main() {
-    let board = BoardBuilder::new().set_total(10).act_on(RandomCenterMarkPair);
-    println!("{board:?}");
-}
