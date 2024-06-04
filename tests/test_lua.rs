fbaad67682fd0c1303996bd791799ffa200ba922
arch machine <lilith.alanis.255@gmail.com>
2024-06-03 20:51:41 -0600

    first lua integration

--- tests/test_lua.rs
+++ tests/test_lua.rs
@@ -1,25 +1,33 @@
 use loteria_engine::engine::*;
 
 const LUA_CODE: &str = "
+local i = 1
 for i=1, 4 do
-    print(\"lua: \", i)
-    local p = board_prototypes[i]
+    local j = 1
     for j=1,16 do
-        p[i] = i
+        board_prototypes[i][j] = j
     end
 end
 ";
 
 #[test]
 fn lua_test() {
-    let mut builder = BoardBuilder::new();
-    builder.set_total_ref(54);
-    builder.set_count_ref(4);
-    builder.act_on_ref(LuaActor::new(LUA_CODE.to_string()));
+    let mut builder = BoardBuilder::new()
+        .act_on(SetTotal(4))
+        .act_on(SetCount(54))
+        .act_on(LuaActor::new(LUA_CODE));
 
-    let boards = builder.generate_boards();
-    for board in boards{
-        println!("{board}");
+    let boards = builder
+        .generate_tapes()
+        .generate_boards();
+
+
+    let mut output_board = Board::new_copy(Card::Value(1));
+    for i in 0..16{
+        output_board.get_mut(i).set((i + 1) as u8);
+    }
+    for board in &boards{
+        assert_eq!(output_board, boards[0], "comparing boards");
     }
 
 }
