84e87d797f06bd270d8bc483412c4eda0502f2e6
arch machine <lilith.alanis.255@gmail.com>
2024-06-03 21:31:37 -0600

    added comments and fixed tests

--- src/engine/test.rs
+++ src/engine/test.rs
@@ -1,40 +1,51 @@
 use anyhow::*;
 use super::*;
 
-fn test_tape(count: usize) -> Result<()>{
-    let t = BoardBuilder::new().act_on(SetCount(count)).generate_tape();
-    let tape = t.tape;
+// default card count in mexican lotery
+const CARD_COUNT: usize = 54;
+
+fn test_tape(tape: Tape, total: usize) -> Result<()> {
     // check if each 16 card chunck is unique
     println!("tape: ");
-    for i in 0..tape.len(){
-        print!("{:2} ", tape[i]);
+    for i in 0..tape.0.len(){
+        print!("{:2} ", tape.0[i]);
         if i % 16 == 15{
             println!();
         }
     }
     println!();
-    for i in 0..count{
+    for i in 0..total{
         let start = i * 16;
         let end = start + 16;
-        let chunk = &tape[start..end];
+        let chunk = &tape.0[start..end];
         println!("chunk: {chunk:?}");
         let mut count = [ 0; CARD_COUNT];
         for j in start..end{
-            let v = tape[j];
+            let v = tape.0[j];
             if count[v as usize] != 0{
                 return Err(anyhow!("Failed at {i} with {v}"));
             }
             count[v as usize] += 1;
         }
     }
+    Ok(())
+}
+
+fn test_tapes(total: usize) -> Result<()>{
+    let t = BoardBuilder::new()
+        .act_on(SetTotal(total))
+        .generate_tapes();
+    for tape in t.tapes{
+        test_tape(tape, total)?;
+    }
 
     return Ok(());
 }
 
 #[test]
-pub fn test_tapes() -> Result<()>{
+pub fn test_tapes_sizes() -> Result<()>{
     for i in 0..100{
-        test_tape(i)?
+        test_tapes(i)?
     }
 
     Ok(())
@@ -56,10 +67,11 @@ fn test_board(board: &Board) -> anyhow::Result<()>{
     return Ok(());
 }
 
-fn test_boards(count: usize) -> Result<()>{
+fn test_boards(total: usize) -> Result<()>{
     let boards = BoardBuilder::new()
-        .act_on(SetCount(count))
-        .generate_tape()
+        .act_on(SetTotal(total))
+        .act_on(SetCount(CARD_COUNT))
+        .generate_tapes()
         .generate_boards();
 
     for board in boards.iter(){
